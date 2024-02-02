c9c3cd905bcc155b2592239be00becdd14f01b79
Anezijane <ahnnieanezi@gmail.com>
2024-02-02 18:18:53 +0100

    add active toggle buttons

--- src/main.js
+++ src/main.js
@@ -8,16 +8,17 @@ function showContent(pageId) {
     // Remove active class from all buttons
     var buttons = document.getElementsByClassName('btn');
     for (var i = 0; i < buttons.length; i++) {
-      buttons[i].classList.toggle('bg-blue-500');
+      buttons[i].classList.remove('bg-cyan-200');
     }
 
     // Show the selected content section
     document.getElementById(pageId).style.display = 'block';
 
     // Add active class to the clicked button
-    var button = document.querySelector('.btn' + pageId.slice(-1));
-    if (button) {
-      button.classList.add('bg-cyan-200');
-    }
+    document.querySelectorAll('.btn').forEach(button => {
+      if (button.id === pageId + '-btn'){
+        button.classList.toggle('bg-cyan-200');
+      }
+    });
   
   }
